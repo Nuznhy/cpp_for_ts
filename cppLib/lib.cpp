1ecf9fe326f69df6b240c0ffc6180506d81f7838
nuzhnyi <nuzhny@gmail.com>
2023-03-15 12:02:27 +0200

    performance test

--- cppLib/lib.cpp
+++ cppLib/lib.cpp
@@ -2,5 +2,4 @@
 
 std::string calculate(float f_number, double d_number, int i_number, std::string str) {
     return std::to_string(f_number * 2) + ' ' + std::to_string(d_number / 2) + ' ' + std::to_string(i_number + 2) + ' ' + str;
-
 }
--- index.ts
+++ index.ts
@@ -2,5 +2,24 @@ const libModule = require('./build/Release/calc.node');
 
 console.log('smts')
 
+const nativeTSCalcFunction = (float: number, double: number, int: number, str: string) => {
+    return `${float*2} ${double/2} ${int + 2} ${str}`
+}
+
+// performance test
 console.log('Exports : ', libModule);
+let startTime = performance.now();
 console.log('calc() :', libModule.calculateFunction(4.222, 8.21409271895712894, 2, 'result'));
+let endTime = performance.now()
+console.log(`Native C++ took ${endTime-startTime} milliseconds`)
+
+startTime = performance.now();
+console.log('nativeTSCalcFunction(): ', nativeTSCalcFunction(4.222, 8.21409271895712894, 2, 'result'))
+endTime = performance.now()
+console.log(`Native TS took ${endTime-startTime} milliseconds`)
+// error handling
+try {
+    console.log('calc() 2: ', libModule.calculateFunction());
+} catch (e: any) {
+    console.error(e.message);
+}
